4632224aad2ece554a045c575921d67539dd3f9c
Cpulsipher24 <collintpulsipher@gmail.com>
2024-02-14 12:05:35 -0700

    Adjusted src-sw.js

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,36 +1,39 @@
-const {
-  precacheAndRoute,
-  registerRoute,
-} = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-// Precache all assets
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Cache page navigation requests
-registerRoute(
-  ({ request }) => request.mode === 'navigate',
-  new StaleWhileRevalidate({
-    cacheName: 'pages',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 50,
-      }),
-    ],
-  })
-);
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
 
-// Cache other assets using CacheFirst strategy
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
 registerRoute(
-  /\.(?:js|css|png|jpg|jpeg|svg|gif)$/,
-  new CacheFirst({
-    cacheName: 'assets',
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
     plugins: [
-      new ExpirationPlugin({
-        maxEntries: 100,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
       }),
     ],
   })
